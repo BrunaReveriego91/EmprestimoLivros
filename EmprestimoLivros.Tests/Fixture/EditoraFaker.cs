5b37b61b9f6e34ca1e39631a00900258c814ab9f
Bruna Reveriego <94184681+BrunaReveriego91@users.noreply.github.com>
2024-07-05 18:12:42 -0300

    criacao teste de integracao

---
+++ EmprestimoLivros.Tests/Fixture/EditoraFaker.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using EmprestimoLivros.Domain.Entities;
+
+namespace EmprestimoLivros.Tests.Fixture
+{
+    public static class EditoraFaker
+    {
+        public static List<Editora> GerarEditoraFake(int qtdeRegistros = 1)
+        {
+            var editoraFaker = new Faker<Editora>("pt_BR")
+                .RuleFor(x => x.Id, f => f.Random.Int(1, 100))
+                .RuleFor(x => x.Nome, f => f.Random.String2(50))
+                .RuleFor(x => x.CNPJ, f => f.Company.Cnpj());
+
+
+            var editorasFaker = editoraFaker.Generate(qtdeRegistros);
+            return editorasFaker;
+
+        }
+    }
+}
---
+++ EmprestimoLivros.Tests/IntegrationTests/EditoraIntegrationTest.cs
@@ -0,0 +1,26 @@
+﻿using EmprestimosLivros.API;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace EmprestimoLivros.Tests.IntegrationTests
+{
+    public class EditoraIntegrationTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        readonly HttpClient _httpClient;
+        public EditoraIntegrationTest(WebApplicationFactory<Startup> fixture)
+        {
+            _httpClient = fixture.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("/Editora")]
+        public async Task ListarEditoras(string url)
+        {
+            //Arrange & Act
+            var response = await _httpClient.GetAsync(url);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
