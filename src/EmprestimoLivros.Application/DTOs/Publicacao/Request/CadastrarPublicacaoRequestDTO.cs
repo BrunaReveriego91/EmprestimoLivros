250d51d3ff3c37b72e729aac66c0dc70c7c7ed1c
Rodrigo Reis <roo_reiis@hotmail.com>
2024-07-12 18:33:26 -0300

    feat: Adicionado atributos faltantes de mapeamento

--- src/EmprestimoLivros.Application/DTOs/Publicacao/Request/CadastrarPublicacaoRequestDTO.cs
+++ src/EmprestimoLivros.Application/DTOs/Publicacao/Request/CadastrarPublicacaoRequestDTO.cs
@@ -8,6 +8,12 @@ namespace EmprestimoLivros.Application.DTOs.Publicacao.Request
 {
     public class CadastrarPublicacaoRequestDTO
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime AnoDeLancamento { get; set; }
+        public string ISBN { get; set; }
+        public string Descricao { get; set; }
+        public string Tags { get; set; }
 
     }
 }
--- src/EmprestimoLivros.Application/DTOs/Usuario/Request/CadastrarUsuarioRequestDTO.cs
+++ src/EmprestimoLivros.Application/DTOs/Usuario/Request/CadastrarUsuarioRequestDTO.cs
@@ -15,5 +15,6 @@ namespace EmprestimoLivros.Application.DTOs.Usuario.Request
         public string TipoUsuario { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
